0e8384cdd9048809325accd9091a45aafb9f2b9e
vivek212 <vivek212@users.noreply.github.com>
2020-01-11 11:30:38 +0530

    Add project files.

---
+++ ProductsInfo/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProductsInfo.Models;
+using System.Data.Entity;
+
+namespace ProductsInfo.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ProductEntities db = new ProductEntities();
+        // GET: Products
+        public ActionResult GetAll()
+        {            
+            return View(db.Products.ToList());
+        }
+    }
+}
